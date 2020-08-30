d6b71017142fd591ce9147fac46aba8ff6d180ab
keremcanb <keremcan@gmail.com>
2020-08-31 01:57:58 +0300

    Added api

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,11 +1,14 @@
+/* eslint-disable consistent-return */
 /* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends Component {
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
 
   renderError({ error, touched }) {
     if (touched && error) {
@@ -61,7 +64,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);

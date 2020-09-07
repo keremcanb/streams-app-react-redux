317620ea38b830c74e3762809886bca69527c848
keremcanb <keremcan@gmail.com>
2020-09-07 20:09:20 +0300

    Show stream

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -33,7 +33,9 @@ const StreamList = ({ fetchStreams, currentUserId, streams, isSignedIn }) => {
           {renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div>
-            <div className="content">{stream.title}</div>
+            <Link to={`/streams/${stream.id}`} className="header">
+              {stream.title}
+            </Link>
             <div className="description">{stream.description}</div>
           </div>
         </div>
--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+const StreamShow = ({ fetchStream, match, stream }) => {
+  useEffect(() => {
+    fetchStream(match.params.id);
+  }, [fetchStream, match]);
+
+  return stream ? (
+    <div>
+      <h1>{stream.title}</h1>
+      <h5>{stream.description}</h5>
+    </div>
+  ) : (
+    <div>Loading...</div>
+  );
 };
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);

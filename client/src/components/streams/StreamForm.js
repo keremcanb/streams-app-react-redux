34262ab708884742aca7d332d1d468d1212aec62
keremcanb <keremcan@gmail.com>
2020-09-09 05:09:18 +0300

    refactor

--- client/src/components/streams/StreamForm.js
+++ client/src/components/streams/StreamForm.js
@@ -1,50 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-class StreamForm extends Component {
-  onSubmit = (formValues) => {
-    this.props.onSubmit(formValues);
-  };
-
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  }
-
-  renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
-
+const renderError = ({ error, touched }) => {
+  if (touched && error) {
     return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
+      <div className="ui error message">
+        <div className="header">{error}</div>
       </div>
     );
+  }
+};
+
+const renderInput = ({ input, label, meta }) => {
+  const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+
+  return (
+    <div className={className}>
+      <label>{label}</label>
+      <input {...input} autoComplete="off" />
+      {renderError(meta)}
+    </div>
+  );
+};
+
+const StreamForm = ({ onSubmit, handleSubmit }) => {
+  const onSubmitHandle = (formValues) => {
+    onSubmit(formValues);
   };
 
-  render() {
-    return (
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form error"
-      >
-        <Field name="title" component={this.renderInput} label="Enter Title" />
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Enter Description"
-        />
-        <button className="ui button primary">Submit</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleSubmit(onSubmitHandle)} className="ui form error">
+      <Field name="title" component={renderInput} label="Enter Title" />
+      <Field
+        name="description"
+        component={renderInput}
+        label="Enter Description"
+      />
+      <button className="ui button primary">Submit</button>
+    </form>
+  );
+};
 
 const validate = (formValues) => {
   const errors = {};
--- readme.md
+++ readme.md
@@ -4,4 +4,4 @@ A video streaming app using CRUD pattern
 
 Libraries used in this project are React, Redux, Redux Thunk, Redux Form, Axios, Lodash
 
-Part of the Modern React with Redux course by Stephen Grider.
+Part of the Modern React with Redux course by Stephen Grider
